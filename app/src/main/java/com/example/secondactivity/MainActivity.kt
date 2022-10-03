ae79db660d8fa8e4c3d78188069812d0c0aac403
youssefchlendi <youssef.chlendi@gmail.com>
2022-10-03 16:02:04 +0100

    Use radio buttons

--- app/src/main/java/com/example/secondactivity/MainActivity.kt
+++ app/src/main/java/com/example/secondactivity/MainActivity.kt
@@ -3,45 +3,40 @@ package com.example.secondactivity
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.TextView
+import android.widget.*
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class MainActivity : AppCompatActivity() {
-    lateinit var choix : TextView;
-    lateinit var yes : CheckBox;
-    lateinit var no : CheckBox;
-    lateinit var button : Button;
+    lateinit var root : ConstraintLayout;
+    lateinit var button: Button;
+    lateinit var red:RadioButton;
+    lateinit var yellow : RadioButton;
+    lateinit var green : RadioButton;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        choix = findViewById(R.id.choix);
-        yes = findViewById(R.id.checkBox);
-        no = findViewById(R.id.checkBox2);
+        root = findViewById(R.id.root);
+        red = findViewById(R.id.r);
+        yellow = findViewById(R.id.y);
+        green = findViewById(R.id.g);
         button = findViewById(R.id.button);
 
         button.setOnClickListener {
-            choix.setTextColor(Color.BLACK);
-            if(yes.isChecked){
-                choix.text = "Yes i have";
-                no.isChecked = false;
-            }else if (no.isChecked){
-                choix.text = "No i don't";
-                yes.isChecked = false;
+            if(red.isChecked){
+                root.setBackgroundColor(Color.RED);
+            }else if (yellow.isChecked){
+                root.setBackgroundColor(Color.YELLOW);
+            }else if (green.isChecked){
+                root.setBackgroundColor(Color.GREEN);
             }else{
-                choix.setTextColor(Color.RED);
-                choix.text = "You must pick a choice";
+
             }
+
         }
-        yes.setOnClickListener {
-            if(yes.isChecked){
-                no.isChecked = false;
-            }
-        }
-        no.setOnClickListener {
-            if (no.isChecked){
-                yes.isChecked = false;
-            }
-        }
+
+
+
     }
 }
